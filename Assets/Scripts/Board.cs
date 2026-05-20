a67913c89b6d2fe607448e2a3534a74f7acbf44c
Andy-Sheng314 <103331109+Andy-Sheng314@users.noreply.github.com>
2026-05-20 15:51:17 -0700

    Add instantiation of roads and buildings using new coordinate system
    Also reorganized assets folder to include "Prefab" folder with existing
    hex prefab along with placeholder road and building prefabs

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public enum TileType { Wheat, Stone, Brick };
+public enum TileType { Water, Wheat, Stone, Brick };
 public class HexPos { 
     public int q, r; 
     public HexPos(int q_in, int r_in)
@@ -13,15 +12,19 @@ public class HexPos {
     }
 };
 
+[RequireComponent(typeof(Transform))]
 public class Board : MonoBehaviour
 {       
     private Dictionary<TileType, int> inventory = new();
     private Dictionary<HexPos, Hex> board = new();
 
-    [SerializeField]
-    private GameObject hex_tile;
+    [SerializeField] private GameObject hex_tile;
+    [SerializeField] private GameObject road;
+    [SerializeField] private GameObject building;
 
     public int R = 2;
+    public float scale = 1.0f; // size of tiles
+    public float padding = 0.1f; // space between tiles
 
     void Awake()
     {   
@@ -31,22 +34,83 @@ public class Board : MonoBehaviour
             inventory.Add((TileType) type, 0);
         }
         
-        for (int q = 1-R; q < R; ++q)
-        {
-            for (int r = 1-R; r < R; ++r)
-            {
-                if (Math.Abs(-q-r) < R)
-                {
-                    // Debug.Log($"q: {q}, r: {r}");
+        // initialize game pieces
+        InstantiateHexes();
+        InstantiateRoads();
+        InstantiateBuildings();
+    }
 
-                    HexPos hex_pos = new(q, r);
+    void InstantiateHexes()
+    {
+        for (int q = -R; q <= R; ++q)
+        {
+            for (int r = -R; r <= R; ++r)
+            {
+                int s = Math.Abs(-q-r);
+                if (s <= R)
+                {
+                    HexPos hex_pos = new(6*q, 6*r);
                     Vector3 pixel_pos = CalcPixelPos(hex_pos);
 
                     GameObject instance = Instantiate(hex_tile, 
                                           pixel_pos, 
-                                          Quaternion.identity);
+                                          Quaternion.identity,
+                                          GetComponent<Transform>());
                     Hex hex = instance.GetComponent<Hex>();
-                    hex.Init(hex_pos, TileType.Wheat, Color.white);
+                    board.Add(hex_pos, hex);
+                    
+                    // just for fun
+                    // System.Random rand = new System.Random();
+                    // Color rand_colour = new Color((float)rand.NextDouble(), 
+                    //                              (float)rand.NextDouble(),
+                    //                              (float)rand.NextDouble());
+
+                    // check if border
+                    if (Math.Abs(q) < R && Math.Abs(r) < R && Math.Abs(s) < R)
+                        hex.Init(hex_pos, TileType.Wheat, 
+                        Color.white, scale);
+                    else
+                        hex.Init(hex_pos, TileType.Water, 
+                        new Color(0.54f, 0.65f, 0.66f), scale);
+                }
+            }
+        }
+    }
+
+    void InstantiateRoads()
+    {
+        for (int q = 1-2*R; q < 2*R; ++q)
+        {
+            for (int r = 1-2*R; r < 2*R; ++r)
+            {
+                int s = -q-r;
+                if (Math.Abs(s) < 2*R 
+                    && (q % 2 != 0 || r % 2 != 0 || s % 2 != 0))
+                {
+                    HexPos hex_pos = new(3*q, 3*r);
+                    Vector3 pixel_pos = CalcPixelPos(hex_pos);
+
+                    Instantiate(road, pixel_pos, Quaternion.identity, 
+                                GetComponent<Transform>());
+                }
+            }
+        }
+    }
+
+    void InstantiateBuildings()
+    {
+        for (int q = 1-3*R; q < 3*R; ++q)
+        {
+            for (int r = 1-3*R; r < 3*R; ++r)
+            {
+                int s = -q-r;
+                if (Math.Abs(s) < 3*R && q*r*s % 3 != 0)
+                {
+                    HexPos hex_pos = new(2*q, 2*r);
+                    Vector3 pixel_pos = CalcPixelPos(hex_pos);
+
+                    Instantiate(building, pixel_pos, Quaternion.identity, 
+                                GetComponent<Transform>());
                 }
             }
         }
@@ -54,8 +118,9 @@ public class Board : MonoBehaviour
 
     Vector3 CalcPixelPos(HexPos hex_pos)
     {
-        float x = (float)(Math.Sqrt(3)*hex_pos.q + Math.Sqrt(3)/2 * hex_pos.r);
-        float y = (float)(1.5*hex_pos.r);
+        float x = (scale + padding) * (float)(Math.Sqrt(3) * hex_pos.q 
+                    + Math.Sqrt(3) / 2 * hex_pos.r) / 1.5f / 6f;
+        float y = (scale + padding) * hex_pos.r / 6f;
         return new Vector3(x, y, 0);
     }
 
--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Hex : MonoBehaviour
 {
     private HexPos hex_pos;
     private TileType tile_type;
 
-    [SerializeField]
-    private SpriteRenderer sprite_renderer;
+    [SerializeField] private SpriteRenderer sprite_renderer;
+    [SerializeField] private Transform hex_transform;
 
-    public void Init(HexPos hex_pos_in, TileType tile_type_in, Color colour)
+    public void Init(HexPos hex_pos_in, TileType tile_type_in, Color colour, 
+                     float scale)
     {
         hex_pos = hex_pos_in;
         tile_type = tile_type_in;
         sprite_renderer.color = colour;
+        hex_transform.localScale = new Vector3(scale, scale, 1);
     }
 }
